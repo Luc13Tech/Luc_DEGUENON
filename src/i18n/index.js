d75019cc54850511f91eb3d14868c0ddfec7fc5b
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-16 13:14:33 +0000

    Create index.js

---
+++ src/i18n/index.js
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const resources = {
+  fr: {
+    translation: {
+      languageName: "Français",
+    },
+  },
+
+  en: {
+    translation: {
+      languageName: "English",
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+
+  lng: "fr",
+
+  fallbackLng: "fr",
+
+  interpolation: {
+    escapeValue: false,
+  },
+
+  returnNull: false,
+});
+
+export default i18n;
