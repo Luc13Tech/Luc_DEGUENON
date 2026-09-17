94ea7588d6a77ddff06daa70cf24caaaaa2a7cee
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-17 09:43:29 +0000

    Add Container component for layout management

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({
+  children,
+  className = "",
+  as: Tag = "div",
+}) {
+  return (
+    <Tag className={`container ${className}`.trim()}>
+      {children}
+    </Tag>
+  );
+}
