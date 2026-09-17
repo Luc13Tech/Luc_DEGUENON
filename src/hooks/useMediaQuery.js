171f7515f122e800405b2bc098ef67845fcbca05
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-17 15:25:00 +0000

    Create useMediaQuery.js

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+export default function useMediaQuery(query) {
+  const getMatches = () => {
+    if (typeof window === "undefined") {
+      return false;
+    }
+
+    return window.matchMedia(query).matches;
+  };
+
+  const [matches, setMatches] = useState(getMatches);
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return undefined;
+    }
+
+    const mediaQuery = window.matchMedia(query);
+
+    const handleChange = (event) => {
+      setMatches(event.matches);
+    };
+
+    setMatches(mediaQuery.matches);
+
+    mediaQuery.addEventListener(
+      "change",
+      handleChange
+    );
+
+    return () => {
+      mediaQuery.removeEventListener(
+        "change",
+        handleChange
+      );
+    };
+  }, [query]);
+
+  return matches;
+}
