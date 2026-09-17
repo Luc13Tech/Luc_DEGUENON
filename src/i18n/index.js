c2f67c6f1d3ad09e245a8ecb12677037b24cdf80
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-17 15:36:57 +0000

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,32 +1,30 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-const resources = {
-  fr: {
-    translation: {
-      languageName: "Français",
+import translations from "./translations";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: translations,
+
+    lng: "fr",
+
+    fallbackLng: "fr",
+
+    supportedLngs: ["fr", "en"],
+
+    interpolation: {
+      escapeValue: false,
     },
-  },
 
-  en: {
-    translation: {
-      languageName: "English",
+    returnNull: false,
+
+    returnEmptyString: false,
+
+    react: {
+      useSuspense: false,
     },
-  },
-};
-
-i18n.use(initReactI18next).init({
-  resources,
-
-  lng: "fr",
-
-  fallbackLng: "fr",
-
-  interpolation: {
-    escapeValue: false,
-  },
-
-  returnNull: false,
-});
+  });
 
 export default i18n;
