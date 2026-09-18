1bebac0dd084ab4f8c0e2c63e2e60a096fb3ef36
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-18 14:38:41 +0000

    Export admin-related functions from adminApi

---
+++ src/admin/services/index.js
@@ -0,0 +1,31 @@
+export {
+  adminLogin,
+  adminLogout,
+  getCurrentAdmin,
+
+  getAdminProjects,
+  createProject,
+  updateProject,
+  deleteProject,
+
+  getAdminSkills,
+  createSkill,
+  updateSkill,
+  deleteSkill,
+
+  getAdminServices,
+  createService,
+  updateService,
+  deleteService,
+
+  updateProfile,
+
+  getAdminSettings,
+  updateSettings,
+
+  uploadMedia,
+  getAdminMedia,
+  deleteMedia,
+
+  getAuditLogs,
+} from "./adminApi";
