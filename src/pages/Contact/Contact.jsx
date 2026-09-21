4379a43ea678185de6556cb241ba47a549f29cc7
Luc DEGUENON <lucdeguenon11@gmail.com>
2026-09-21 11:43:28 +0000

    Update Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -14,6 +14,7 @@ import SectionTitle from "../../components/SectionTitle";
 import Button from "../../components/Button";
 import SEO from "../../components/SEO";
 import api from "../../services/api";
+import "./Contact.css";
 
 export default function Contact({ profile }) {
   const { t } = useTranslation();
